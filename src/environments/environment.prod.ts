c9ca0480113efe2a39ef3b643576dc161f304027
alexfloc82 <alexandre.floquet@gmail.com>
2017-09-23 15:33:51 +0200

    parametrización prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAMoVCjNT323p6BJS11o6qiqbJOwH9IXck",
+    authDomain: "ocitv-complutense.firebaseapp.com",
+    databaseURL: "https://ocitv-complutense.firebaseio.com",
+    projectId: "ocitv-complutense",
+    storageBucket: "",
+    messagingSenderId: "319794440492"
+  }
 };
