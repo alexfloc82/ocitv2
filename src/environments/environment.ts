475b0fe19909c49ab46a6129a90328a093dcc27c
alexfloc82 <alexandre.floquet@gmail.com>
2017-09-23 18:49:20 +0200

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  /*dev
+  //dev
   firebase: {
     apiKey: "AIzaSyALka-K-dxDyAfPpJPcmIh0ezX0lQYVrwQ",
     authDomain: "ocitv-dev.firebaseapp.com",
@@ -13,14 +13,14 @@ export const environment = {
     projectId: "ocitv-dev",
     storageBucket: "ocitv-dev.appspot.com",
     messagingSenderId: "709455507086"
-  },*/
+  },
   //prod
-  firebase: {
+  /*firebase: {
     apiKey: "AIzaSyAMoVCjNT323p6BJS11o6qiqbJOwH9IXck",
     authDomain: "ocitv-complutense.firebaseapp.com",
     databaseURL: "https://ocitv-complutense.firebaseio.com",
     projectId: "ocitv-complutense",
     storageBucket: "",
     messagingSenderId: "319794440492"
-  }
+  }*/
 };
