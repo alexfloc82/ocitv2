2c5f477804267e876cb88840ed2ce16ba6bbe87b
alexfloc82 <alexandre.floquet@gmail.com>
2018-02-03 14:14:27 +0100

    completar analisis

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 //import {DashboardService} from './dashboard.service';
-import {Tarea} from '../shared/datamodel';
+import { Tarea } from '../shared/datamodel';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Subject } from 'rxjs/Subject';
 
@@ -11,10 +11,10 @@ import { Subject } from 'rxjs/Subject';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  selectedItem:string;
-  semestre:FirebaseListObservable<any[]>;
-  query:any;
-  loader:boolean;
+  selectedItem: string;
+  semestre: FirebaseListObservable<any[]>;
+  query: any;
+  loader: boolean;
   piezas: any[];
   informativos: any[];
   lugares: any[];
@@ -22,47 +22,51 @@ export class DashboardComponent implements OnInit {
   quienes: any[];
   dquienes: any[];
   dques: any[];
+  retoricas: any[];
+  rotulos: any[];
+  origenes: any[];
 
   combos: any;
 
   constructor(
     //private dashService : DashboardService,
     private db: AngularFireDatabase
-  ) { 
-    
+  ) {
+
     this.semestre = this.db.list('/values/semestre');
-    this.query = {semestre:""};
+    this.query = { semestre: "" };
 
   }
 
   ngOnInit() {
-    this.loader = true; 
+    this.loader = true;
     this.selectedItem = 'informativos';
-    this.query = {semestre:""};
-    this.db.object('/values').subscribe(combos => 
-      {
-        this.combos = combos;
-        this.emitNormTarea();
-        this.loader = false; 
-      });
-     
+    this.query = { semestre: "" };
+    this.db.object('/values').subscribe(combos => {
+      this.combos = combos;
+      this.emitNormTarea();
+    });
+
   }
 
   selectItem(selectedItem: any) {
     this.selectedItem = selectedItem;
   }
 
-  performQuery(){
+  performQuery() {
     this.setQuery(this.query);
     this.emitNormTarea();
   }
 
-  formatPiezas(piezas: any[]) {  
+  formatPiezas(piezas: any[]) {
     this.lugares = [];
     this.locals = [];
     this.quienes = [];
     this.dquienes = [];
     this.dques = [];
+    this.retoricas = [];
+    this.rotulos = [];
+    this.origenes = [];
 
     piezas.forEach(pieza => {
       pieza.lugares.forEach(lugar => {
@@ -73,60 +77,62 @@ export class DashboardComponent implements OnInit {
         this.lugares.push(lugar);
       });
 
-      pieza.localidades.forEach(local =>{
+      pieza.localidades.forEach(local => {
         local['id_tarea'] = pieza.id_tarea;
         this.locals.push(local);
       });
 
-      pieza.quienes.forEach(quien =>{
+      pieza.quienes.forEach(quien => {
         quien['id_tarea'] = pieza.id_tarea;
         this.quienes.push(quien);
       });
 
-      pieza.dquienes.forEach(quien =>{
+      pieza.dquienes.forEach(quien => {
         quien['id_tarea'] = pieza.id_tarea;
         this.dquienes.push(quien);
       });
 
-      pieza.dques.forEach(dque =>{
+      pieza.dques.forEach(dque => {
         dque['id_tarea'] = pieza.id_tarea;
-        dque['tematica']=pieza['catTem-l'];
+        dque['tematica'] = pieza['catTem-l'];
         this.dques.push(dque);
       });
 
-     // pieza['retorica'] = '';
-     // pieza.retorica.forEach(retorica => pieza['retorica']=pieza['retorica'] + ' ' + retorica['retorica-l'])
+      pieza['retorica-l'].forEach(retorica => this.retoricas.push(retorica));
+      pieza['rotulos-l'].forEach(rotulo => this.rotulos.push(rotulo));
+      pieza['origen-l'].forEach(origen => this.origenes.push(origen));
 
     });
 
     this.loader = false;
   }
 
-  emitNormTarea(){
-    if(!this.combos){return null;}
+  emitNormTarea() {
+    this.loader = true;
+    if (!this.combos) { return null; }
     this.db.list('/tareas')
-    .subscribe(tareas => {
-        let fileteredTarea = tareas.filter(a=> (this.query.semestre =="" || this.query.semestre == a.semestre));//prepara el filtro con la query
-        fileteredTarea.forEach((tarea, index, tareas)=>{
-            tarea['cadena-l'] = this.combos.cadena[tarea.cadena];
-            tarea['edicion-l'] = this.combos.edicion[tarea.edicion];
-            tarea['semestre-l'] = this.combos.semestre[tarea.semestre];
-            tarea['duracion'] = (tarea.eh-tarea.bh)*3600 + (tarea.em - tarea.bm)*60 + (tarea.es-tarea.bs);
+      .subscribe(tareas => {
+        let fileteredTarea = tareas.filter(a => (this.query.semestre == "" || this.query.semestre == a.semestre));//prepara el filtro con la query
+        fileteredTarea.forEach((tarea, index, tareas) => {
+          tarea['cadena-l'] = this.combos.cadena[tarea.cadena];
+          tarea['edicion-l'] = this.combos.edicion[tarea.edicion];
+          tarea['semestre-l'] = this.combos.semestre[tarea.semestre];
+          tarea['duracion'] = (tarea.eh - tarea.bh) * 3600 + (tarea.em - tarea.bm) * 60 + (tarea.es - tarea.bs);
         })
         this.informativos = fileteredTarea;
         this.emitNormFicha();
-    });
+      });
   }
 
-  emitNormFicha(){
-    if(!this.combos){return null;}
+  emitNormFicha() {
+    if (!this.combos) { return null; }
     this.db.list('/fichas')
-    .subscribe(fichas => {
-        fichas.forEach((ficha, index, fichas)=>{
-          ficha['tarea'] = this.informativos.filter(a=>ficha.tarea_id==a.$key)[0];
+      .subscribe(fichas => {
+        fichas.forEach((ficha, index, fichas) => {
+          ficha['tarea'] = this.informativos.filter(a => ficha.tarea_id == a.$key)[0];
         });
-        let fileteredFicha = fichas.filter(a=>(this.query.semestre =="" || this.query.semestre == a.tarea.semestre));//prepara el filtro con la query
-        fileteredFicha.forEach((ficha, index, fichas)=>{
+        let fileteredFicha = fichas.filter(a => (a.tarea));//prepara el filtro con la query
+        fileteredFicha.forEach((ficha, index, fichas) => {
           ficha['id_tarea'] = ficha.tarea.id_tarea;
           ficha['genero-l'] = this.combos.genero[ficha.genero];
           ficha['format-l'] = this.combos.format[ficha.format];
@@ -136,93 +142,112 @@ export class DashboardComponent implements OnInit {
           ficha['informador-l'] = this.combos.informador[ficha.informador];
           ficha['zona-l'] = this.combos.zona[ficha.zona];
           ficha['catTem-l'] = this.combos.catTem[ficha.catTem];
-          if(ficha.lugares){
-            ficha.lugares.forEach(lugar =>{
+          ficha['fuente']= (ficha['fuente'])?true:false;
+          ficha['infoAparece']= (ficha['infoAparece'])?true:false;
+          ficha['espConjunto']= (ficha['espConjunto'])?true:false;
+          if (ficha.lugares) {
+            ficha.lugares.forEach(lugar => {
               lugar['pieza'] = ficha.$key;
               lugar['title'] = ficha.title;
-              if(lugar.pais){lugar['pais-l']=this.combos.pais[lugar.pais];}
-              if(lugar.comunidad){lugar['comunidad-l']=this.combos.comunidad[lugar.comunidad];}
+              if (lugar.pais) { lugar['pais-l'] = this.combos.pais[lugar.pais]; }
+              if (lugar.comunidad) { lugar['comunidad-l'] = this.combos.comunidad[lugar.comunidad]; }
             })
           }
-          else{
-            ficha['lugares']=[];
+          else {
+            ficha['lugares'] = [];
           }
-          if(ficha.localidades){
-            ficha.localidades.forEach(lugar =>{
+          if (ficha.localidades) {
+            ficha.localidades.forEach(lugar => {
               lugar['pieza'] = ficha.$key;
               lugar['title'] = ficha.title;
             })
           }
-          else{
-            ficha['localidades']=[];
+          else {
+            ficha['localidades'] = [];
           }
-          if(ficha.dques){
-            ficha.dques.forEach(dque =>{
+          if (ficha.dques) {
+            ficha.dques.forEach(dque => {
               dque['pieza'] = ficha.$key;
               dque['title'] = ficha.title;
-              if(dque.categoria){dque['categoria-l']=this.combos.dqcategoria[dque.categoria];}
+              switch (dque['type']) {
+                case 'Cat':
+                  if (dque.categoria) { dque['categoria-l'] = dque.categoria.name; }
+                  break;
+                case 'Etiqueta':
+                  dque['categoria-l'] = dque.etiqueta;
+                  break;
+              }
+
             })
           }
-          else{
-            ficha['dques']=[];
+          else {
+            ficha['dques'] = [];
           }
-          if(ficha.quienes){
-            ficha.quienes.forEach(quien =>{
+          if (ficha.quienes) {
+            ficha.quienes.forEach(quien => {
               quien['pieza'] = ficha.$key;
               quien['title'] = ficha.title;
-              if(quien.categoria){quien['categoria-l']=this.combos.categoria[quien.categoria];}
+              switch (quien['type']) {
+                case 'Agente':
+                  if (quien.categoria) { quien['categoria-l'] = quien.categoria.name; }
+                  break;
+                case 'Personaje':
+                  quien['categoria-l'] = quien.persona;
+                  break;
+              }
             })
           }
-          else{
-            ficha['quienes']=[];
+          else {
+            ficha['quienes'] = [];
           }
-          if(ficha.dquienes){
-            ficha.dquienes.forEach(dquien =>{
+          if (ficha.dquienes) {
+            ficha.dquienes.forEach(dquien => {
               dquien['pieza'] = ficha.$key;
               dquien['title'] = ficha.title;
-              if(dquien.categoria){dquien['categoria-l']=this.combos.categoria[dquien.categoria];}
+              if (dquien.categoria) { dquien['categoria-l'] = dquien.categoria.name; }
+              dquien['persona'] = dquien.persona;
             })
           }
-          else{
-            ficha['dquienes']=[];
+          else {
+            ficha['dquienes'] = [];
           }
-          if(ficha.origen){
-            ficha['origen-l']=[];
-            ficha.origen.forEach(valor =>{
-              ficha['origen-l'].push({key:valor, title:ficha.title, value:this.combos.origen[valor], pieza: ficha.$key});
+          if (ficha.origen) {
+            ficha['origen-l'] = [];
+            ficha.origen.forEach(valor => {
+              ficha['origen-l'].push({ key: ficha.id_tarea, title: ficha.title, value: this.combos.origen[valor], pieza: ficha.$key });
             })
           }
-          else{
-            ficha['origen']=[];
+          else {
+            ficha['origen-l'] = [];
           }
-          if(ficha.rotulos){
-            ficha['rotulos-l']=[];
-            ficha.rotulos.forEach(valor =>{
-              ficha['rotulos-l'].push({key:valor, title:ficha.title, value:this.combos.rotulos[valor], pieza: ficha.$key})
+          if (ficha.rotulos) {
+            ficha['rotulos-l'] = [];
+            ficha.rotulos.forEach(valor => {
+              ficha['rotulos-l'].push({ key: ficha.id_tarea, title: ficha.title, value: this.combos.rotulos[valor], pieza: ficha.$key })
             })
           }
-          else{
-            ficha['rotulos']=[];
+          else {
+            ficha['rotulos-l'] = [];
           }
-          if(ficha.retorica){
-            ficha['retorica-l']=[];
-            ficha.retorica.forEach(valor =>{
-              ficha['retorica-l'].push({key:valor, title:ficha.title, value:this.combos.retorica[valor], pieza: ficha.$key})
+          if (ficha.retorica) {
+            ficha['retorica-l'] = [];
+            ficha.retorica.forEach(valor => {
+              ficha['retorica-l'].push({ key: ficha.id_tarea, title: ficha.title, value: this.combos.retorica[valor], pieza: ficha.$key })
             })
           }
-          else{
-            ficha['retorica']=[];
+          else {
+            ficha['retorica-l'] = [];
           }
-          ficha['duracion'] = (ficha.ehour-ficha.bhour)*3600 + (ficha.emin - ficha.bmin)*60 + (ficha.esec-ficha.bsec);
+          ficha['duracion'] = (ficha.ehour - ficha.bhour) * 3600 + (ficha.emin - ficha.bmin) * 60 + (ficha.esec - ficha.bsec);
         })
-        this.piezas =fileteredFicha;
+        this.piezas = fileteredFicha;
         this.formatPiezas(this.piezas);
-    });
+        this.loader = false;
+      });
   }
 
-  setQuery(query: any){
-    this.query=query;
+  setQuery(query: any) {
+    this.query = query;
     this.emitNormTarea();
-    this.emitNormFicha();
   }
 }
